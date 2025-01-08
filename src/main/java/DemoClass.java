d341c4f749c8db40b16b0c576ed99c445cd628ab
Vaibhav Gaikwad <GFG19667@gfg.geeksforgeeks.org>
2025-01-08 17:24:39 +0530

    final automation files on 8 jan

--- src/main/java/DemoClass.java
+++ src/main/java/DemoClass.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
 
 public class DemoClass {
+	public static void main(String[] args) {
+		System.out.println("Enter a Numbers: ");
+		int marks[] = new int[5];
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i = 0; i< marks.length; i ++) {
+			marks[i] = sc.nextInt();
+		}
+		display(marks);
+		
+	}
+	public static void display(int m[]) {
+		for(int k:m) {
+			System.out.println(k);
+		}
+	}
 
 }
